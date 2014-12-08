384bdd441750079c2e3d3a6aea9c30d71f2ed9fa
Travellerer <kilianpoessel@googlemail.com>
2014-12-08 10:01:20 +0100

    Arbeit an Main

--- IcyMazeRunner/IcyMazeRunner/Program.cs
+++ IcyMazeRunner/IcyMazeRunner/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,34 @@ namespace IcyMazeRunner
     {
         static void Main(string[] args)
         {
+            RenderWindow Window = new RenderWindow(new VideoMode(800, 600), "IcyMazeRunner");
+
+            Initialize();
+
+            while (Window.IsOpen())
+            {
+                update();
+
+            }
+            
         }
+
+       static void Initialize(){
+           // Spieler, Map, 
+           // später: Gegner, Fallen(Geschosse), Anzeige (Timer/Stoppuhr), HP-Balken
+
+       }
+
+
+       static void update()
+       {
+           // Bewegungsmethode des Spielers, Sichtkreis, bewegliche Mauern (if-Abfrage), Kollision mit Schalter
+           // später: Bewegung der Gegner, Geschosse, Anzeigen, Kollision
+       }
+
+       static void Windowclosed(Object sender, EventArgs e)
+       {
+           ((RenderWindow)sender).Close();
+       }
     }
 }
