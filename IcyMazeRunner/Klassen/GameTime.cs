fce09670e51bdf50d86490ed9248b13afef3247d
Djimon <Djimon@gmx.de>
2014-12-19 00:30:10 +0100

    Map-experimente

---
+++ IcyMazeRunner/Klassen/GameTime.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcyMazeRunner.Klassen
+{
+    class GameTime
+    {
+
+        Stopwatch Watch;
+        public TimeSpan TotalTime;
+        public TimeSpan ElapsedTime;
+
+        public GameTime()
+        {
+            Watch = new Stopwatch();
+            TotalTime = TimeSpan.FromSeconds(0);
+            ElapsedTime = TimeSpan.FromSeconds(0);
+        }
+
+        public void start()
+        {
+            Watch.Start();
+        }
+
+        public void stop()
+        {
+            Watch.Stop();
+            TotalTime = TimeSpan.FromSeconds(0);
+            ElapsedTime = TimeSpan.FromSeconds(0);
+
+        }
+
+        public void update()
+        {
+            ElapsedTime = Watch.Elapsed-TotalTime;
+            TotalTime = Watch.Elapsed;
+        }
+    }
+}
---
+++ IcyMazeRunner/Klassen/Program.cs
@@ -0,0 +1,70 @@
+﻿using IcyMazeRunner.Klassen;
+using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcyMazeRunner
+{
+    class Program
+    {
+
+        //Attribute hier:
+        // Map-Klasse erstellen, walkable als Methode
+
+        static Player Runner;
+
+        static void Main(string[] args)
+        {
+            RenderWindow win = new RenderWindow(new VideoMode(1280, 720), "IcyMazeRunner");
+
+            GameTime time = new GameTime();
+            Map map = new Map();
+            
+            win.Closed += Windowclosed;
+
+            Initialize();
+            time.start();
+
+            while (win.IsOpen())
+            {
+                win.DispatchEvents();
+                time.update();
+                update(map, time);
+                draw(win, time);
+            }
+
+            time.stop();
+        }
+
+       static void Initialize(){
+           Runner = new Player();
+           // Map (45-50 Blöcke untereinander + eventuell einige durchsichtige Blöcke, um einen Hintergrund drum herum darzustellen.
+           // später: Gegner, Fallen(Geschosse), Anzeige (Timer/Stoppuhr), HP-Balken
+       }
+
+
+       static void update(Map map, GameTime time)
+       {
+           Runner.move(map, time);
+           
+           //Sichtkreis, bewegliche Mauern (if-Abfrage), Kollision mit Schalter
+           // später: Bewegung der Gegner, Geschosse, Anzeigen, Kollision
+       }
+
+       static void Windowclosed(Object sender, EventArgs e)
+       {
+           ((RenderWindow)sender).Close();
+       }
+
+       static void draw(RenderWindow win, GameTime time)
+       {
+           win.Clear();
+           Runner.draw(win);
+           win.Display();
+       }
+    }
+}
