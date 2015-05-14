c26093b6f89837840d0db2117366f8ed998ff864
Mortality042 <schiller1996@arcor.de>
2015-05-14 09:32:22 +0200

    - SaveLevel.cs angepasst

--- IcyMazeRunner/Klassen/Gamestates und Gamestruktur/SaveLevel.cs
+++ IcyMazeRunner/Klassen/Gamestates und Gamestruktur/SaveLevel.cs
@@ -3,38 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace IcyMazeRunner.Klassen
 {
     class SaveLevel
     {
+
         const int e = 11;
         const int n = 221;
         const int d = 35;
         /*Zahl verschlüsseln und in einer txt Datei speichern */
         /*Verschlüsselung VZ = KZ^e mod n*/
         /*Entschlüsselung KZ = VZ^d mod n*/
-        public SaveLevel(int level) 
+        public SaveLevel(int number)
         {
-            this.encrypt(level);
+            encrypt(number);
         }
-
         public void encrypt(int number)
         {
-            long encrypt = number ^ e % n;
+            ulong encrypt = (ulong)(Math.Pow(number, e)) % n;
+            Console.WriteLine(encrypt);
             string output = encrypt + "";
-            System.IO.File.WriteAllText(@"..\..\SaveData.txt", output);
-
+            System.IO.File.WriteAllText(@"SaveData.txt", output);
         }
         public int decrypt()
         {
             string input;
-            System.IO.StreamReader file = new System.IO.StreamReader(@"..\..\SaveData.txt");
+            System.IO.StreamReader file = new System.IO.StreamReader(@"SaveData.txt");
             input = file.ReadLine();
-            int tmp = Convert.ToInt32(input);
-            return tmp ^ d % n;
+            BigInteger tmp = new BigInteger(Convert.ToUInt64(input));
+            ulong tmp2 = Convert.ToUInt64(input);
+            return (int)(BigInteger.Pow(tmp, d) % new BigInteger(n));
         }
-            
+
     }
 }
     
