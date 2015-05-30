43fe77efbcda9657027a9087e0ce44b9db99973c
djimon <djimon@gmx.de>
2015-05-30 18:21:30 +0200

    kompassbastelei

--- IcyMazeRunner/Klassen/Gamestates und Gamestruktur/GUI/Kompass.cs
+++ IcyMazeRunner/Klassen/Gamestates und Gamestruktur/GUI/Kompass.cs
@@ -1,4 +1,5 @@
-﻿using SFML.Window;
+﻿using SFML.Graphics;
+using SFML.Window;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -12,12 +13,16 @@ namespace IcyMazeRunner.Klassen.Gamestates_und_Gamestruktur.GUI
     {
        
         System.Drawing.Image needle;
+        Sprite spNeedle;
+        Sprite spnew;
         Vector2f vCompass; //Kompassmittelpunkt
         Vector2f vTarget; // Zielobjekt
 
-        public Kompass(Vector2f midpoint, Vector2f targetPos, Image texturPfad)
+
+        // Konstruktor
+        public Kompass(Vector2f midpoint, Vector2f targetPos, Texture texturPfad)
         {
-            this.needle = texturPfad;
+            this.spNeedle = new Sprite(new Texture(texturPfad));
             this.vCompass = midpoint;
             this.vTarget = targetPos;
         }
@@ -31,7 +36,7 @@ namespace IcyMazeRunner.Klassen.Gamestates_und_Gamestruktur.GUI
         {
             float x = position.X;
             float y = position.Y;
-            return (float)(Math.Atan2(y, x) * 180 / Math.PI);  // drehugnswinkel in (radian) zum zielvector(y-wert,x-wert)
+            return (float)(Math.Atan2(y, x) * 180 / Math.PI);  // drehugnswinkel (in grad *180/PI) zum zielvector(y-wert,x-wert)
         }
 
 
@@ -39,27 +44,41 @@ namespace IcyMazeRunner.Klassen.Gamestates_und_Gamestruktur.GUI
         // name="oldBitmap"  >Bitmap grafik
         // name="angle"  >winkel in Gradmaß
 
-        private static Bitmap RotateImageByAngle(System.Drawing.Image oldBitmap, float angle)
+        //private static Bitmap RotateImageByAngle(System.Drawing.Image oldBitmap, float angle)
+        //{
+        //    var newBitmap = new Bitmap(oldBitmap.Width, oldBitmap.Height);
+        //    newBitmap.SetResolution(oldBitmap.HorizontalResolution, oldBitmap.VerticalResolution);
+        //    var graphics = Graphics.FromImage(newBitmap);
+        //    graphics.TranslateTransform((float)oldBitmap.Width / 2, (float)oldBitmap.Height / 2);
+        //    graphics.RotateTransform(angle);
+        //    graphics.TranslateTransform(-(float)oldBitmap.Width / 2, -(float)oldBitmap.Height / 2);
+        //    graphics.DrawImage(oldBitmap, new Point(0, 0));
+        //    return newBitmap;
+        //}
+
+        static Sprite RotateImageByAngle(Sprite spPic, float angle)
         {
-            var newBitmap = new Bitmap(oldBitmap.Width, oldBitmap.Height);
-            newBitmap.SetResolution(oldBitmap.HorizontalResolution, oldBitmap.VerticalResolution);
-            var graphics = Graphics.FromImage(newBitmap);
-            graphics.TranslateTransform((float)oldBitmap.Width / 2, (float)oldBitmap.Height / 2);
-            graphics.RotateTransform(angle);
-            graphics.TranslateTransform(-(float)oldBitmap.Width / 2, -(float)oldBitmap.Height / 2);
-            graphics.DrawImage(oldBitmap, new Point(0, 0));
-            return newBitmap;
+            //Sprite spOldPic = spPic;
+            Sprite spNewPic = new Sprite(spPic);
+            spNewPic.Rotation = angle;
+            
+
+            return spNewPic;
         }
+
 
         public void update()
         {
             if (getWinkel(getVector(vCompass, vTarget)) != 0)
             {
-                RotateImageByAngle(needle, getWinkel(getVector(vCompass, vTarget)));
+                Sprite spnew = RotateImageByAngle(spNeedle, getWinkel(getVector(vCompass, vTarget)));
             }
         }
-         //  !!!~~~ DRAW ~~~!!!
- 
+
+        public void draw(RenderWindow win) 
+        {
+             win.Draw(spnew);
+        }
 
     }
 }
